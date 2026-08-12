da972467bb30755568e64bba6f12a6edcc7e4501
Soham Mule <sohammule09@gmail.com>
2026-08-12 13:13:24 +0100

    started-auth-service

---
+++ website-tracking-backend/src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { All, Controller, Req, Res } from "@nestjs/common";
+import { Request, Response } from 'express';
+import { toNodeHandler } from "better-auth/node"
+
+
+@Controller("/api/auth")
+export class AuthController {
+    private handler = toNodeHandler(auth)
+
+    @All("*")
+    async handleAuth(@Req() req: Request, @Res() res: Response) {
+        return this.handler(req, res)
+    }
+}
